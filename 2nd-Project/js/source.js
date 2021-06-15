d941d9233080d26b6518c106ff9e7aca2a59a691
VolodymyrLiubashenko <82656913+VolodymyrLiubashenko@users.noreply.github.com>
2021-06-15 16:01:34 +0300

    Add files via upload

---
+++ 2nd-Project/js/source.js
@@ -0,0 +1,79 @@
+
+let DataArr= [
+	{
+		h4: 'Threat Intelligence',
+		p: 'Our team is part of a global network of public and private sector businesses and agencies that share the latest intelligence on emerging threats, attacks, and alerts. We never share the names of companies or customers and we are never in the dark.',
+		img:'images/solve/1.jpg'
+	},
+	{
+		h4: 'Traffic Blindspots',
+		p: 'We keep a close eye on all the comings and goings on every network. Monitoring activity from your infrastructure, desktops, services, endpoints, and other sources, we can detect openings that could leave you vulnerable to threats.',
+		img: 'images/solve/2.jpg'
+	},
+	{
+		h4: 'Abnormal Activity',
+		p: 'Patterns exist in every network. Our platform synthesizes every byte of data to detect anomalies that could indicate a threat. A single outlier in the system can mean the difference between a simple alert and a system failure.',
+		img: 'images/solve/3.jpg'
+	},
+	{
+		h4: 'System Error',
+		p: 'Sometimes, an anomaly is just that. An outlier in a larger pattern that doesn’t indicate a larger attack. It takes a human to know the difference between danger and random data. Our team of security professionals is trained to do just that.',
+		img: 'images/solve/4.jpg'
+	},
+	{	
+		h4:'Missed Connections',
+		p: 'When we find trouble, we don’t just send a simple report. We send a comprehensive gameplan that outlines the severity of the threat, all the technical information you need, and the steps to seal off your vulnerabilities and stop the threat. Plus, we include a report in plain English that summarizes the event simply and succinctly',
+		img: 'images/solve/5.jpg'
+	},
+	{
+		h4: 'Waiting Times',
+		p: 'Our personnel are always available by phone and email to answer any questions you might have. In the event of a breach, we will be there to document every bit of data and assist you with reporting the attack to both internal staff and any governing agencies that need to be informed. ',
+		img: 'images/solve/6.jpg'
+	},
+	{
+		h4: 'Radio Silence',
+		p: 'Some firms might be comfortable with reaching out only when threats appear, but we believe that cybersecurity is a full-time job. That’s why we perform quarterly vulnerability scans to make sure threats don’t slip through the cracks, providing every client with executive reports that outline the progress that has been made and where extra resources are needed.',
+		img: 'images/solve/7.jpg'
+	},
+	{
+		h4: 'Up To Date Solutions',
+		p: 'Every company has different needs. That’s why our executive reports provide our clients with an opportunity to keep us up-to-date on new software, systems, and infrastructure that reshapes the equation. This allows us to provide critical services that are tailored to your specific business, when and where you need them most.',
+		img: 'images/solve/8.jpg'
+	},
+	{
+		h4: 'Test Anxiety',
+		p: 'We believe that you shouldn’t be caught off-guard on game day. It’s just one reason why we practice penetration testing. Performing an actual, controlled attack on your system, we can determine if the measures in place are able to defend against data breaches in the event of a real threat.',
+		img: 'images/solve/9.jpg'
+	},
+]
+
+let dataLogoArray = ["lms","Bsron","Calor","TuskVentures-black","vayner"]
+let dataPositionArray = ["0%","22.5%","45%","67.5%","90%"]
+let dataPositionArrayMobile = ['0%','40%','80%']
+let dataReviewArray = [
+	{
+		p:'“Skout helped us bring our security and operations forward dramatically by being so responsive and detail-oriented. The whole team is really great to work with.”',
+		cite:'— Yoni Rechtman',
+		img:'images/logo-reviews/lms-black.png',
+	},
+	{
+		p:'“Skout helped us bring our security and operations forward dramatically by being so responsive and detail-oriented. The whole team is really great to work with.”',
+		cite:'— Yoni Rechtman',
+		img:'images/logo-reviews/Bsron.png',
+	},
+	{
+		p:'“Skout helped us bring our security and operations forward dramatically by being so responsive and detail-oriented. The whole team is really great to work with.”',
+		cite:'— Yoni Rechtman',
+		img:'images/logo-reviews/Calor.png',
+	},
+	{
+		p:'“Skout helped us bring our security and operations forward dramatically by being so responsive and detail-oriented. The whole team is really great to work with.”',
+		cite:'— Yoni Rechtman',
+		img:'images/logo-reviews/TuskVentures-black.png',
+	},
+	{
+		p:'“Skout helped us bring our security and operations forward dramatically by being so responsive and detail-oriented. The whole team is really great to work with.”',
+		cite:'— Yoni Rechtman',
+		img:'images/logo-reviews/vayner.png',
+	}
+]
